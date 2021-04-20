9ed2df3506f6126844f7bac2af422bc2d8a6a96d
Tom Wagg <twagg@college.harvard.edu>
2021-04-20 13:40:10 -0400

    add new sims

--- simulation/slurm/run_all.sh
+++ simulation/slurm/run_all.sh
@@ -3,9 +3,9 @@ python generate_scripts.py
 
 for type in BHBH BHNS NSNS
 do
-    for variation in fiducial optimistic alpha0_5 alpha2_0 unstableCaseBB massTransferEfficiencyFixed_0_25 massTransferEfficiencyFixed_0_5 massTransferEfficiencyFixed_0_75 ccSNkick_100km_s ccSNkick_30km_s noBHkick rapid maxNSmass2_0 maxNSmass3_0 noPISN
+    for variation in fiducial optimistic alpha0_5 alpha2_0 unstableCaseBB massTransferEfficiencyFixed_0_25 massTransferEfficiencyFixed_0_5 massTransferEfficiencyFixed_0_75 ccSNkick_100km_s ccSNkick_30km_s noBHkick rapid maxNSmass2_0 maxNSmass3_0 noPISN wolf_rayet_multiplier_0_1 wolf_rayet_multiplier_5
     do
         sbatch --array=1-50 scripts/${type}_${variation}.sh
-        sleep 10
+        sleep 1
     done
 done
