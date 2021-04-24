0b080efb775091ce5586f1e9446f78bc821ee5f7
Tom Wagg <twagg@college.harvard.edu>
2021-04-24 14:55:47 -0400

    add verification binaries to plot

---
+++ simulation/notebooks/verification_binaries.py
@@ -0,0 +1,62 @@
+import astropy.units as u
+
+
+"""A list of the LISA verification binaries from Kupfer+18 with SNR > 7"""
+
+verification_binaries = [
+    {
+        "label": "HM Cnc",
+        "f_GW": 6.22 * u.mHz,
+        "snr": 211.1,
+    },
+    {
+        "label": "V407 Vul",
+        "f_GW": 3.51 * u.mHz,
+        "snr": 169.7,
+    },
+    {
+        "label": "ES Cet",
+        "f_GW": 3.22 * u.mHz,
+        "snr": 154.3,
+    },
+    {
+        "label": "SDSS J135154.46–064309.0",
+        "f_GW": 2.12 * u.mHz,
+        "snr": 21.8,
+    },
+    {
+        "label": "AM CVn",
+        "f_GW": 1.94 * u.mHz,
+        "snr": 101.2,
+    },
+    {
+        "label": "SDSS J190817.07+394036.4",
+        "f_GW": 1.84 * u.mHz,
+        "snr": 20.3,
+    },
+    {
+        "label": "HP Lib",
+        "f_GW": 1.81 * u.mHz,
+        "snr": 43.7,
+    },
+    {
+        "label": "CR Boo",
+        "f_GW": 1.36 * u.mHz,
+        "snr": 21.9,
+    },
+    {
+        "label": "V803 Cen",
+        "f_GW": 1.25 * u.mHz,
+        "snr": 26.2,
+    },
+    {
+        "label": "SDSS J065133.34+284423.4",
+        "f_GW": 2.61 * u.mHz,
+        "snr": 90.1,
+    },
+    {
+        "label": "SDSS J093506.92+441107.0",
+        "f_GW": 1.68 * u.mHz,
+        "snr": 44.9,
+    },
+]
