1be168f035ecd68cb40d348c034548d1cbf73a65
Tom Wagg <twagg@college.harvard.edu>
2021-06-19 17:21:01 -0400

    add simple mw script to generation

--- simulation/slurm/generate_scripts.py
+++ simulation/slurm/generate_scripts.py
@@ -3,7 +3,9 @@ sys.path.append("../src/")
 from variations import variations
 
 
-def create_file(bt, file):
+def create_file(bt, file, simple_mw=False):
+    script_file = "scripts/{}_{}.sh".format(bt, file)
+
     lines = []
     lines.append("#!/bin/bash\n")
     lines.append("#SBATCH -J {}_{}\n".format(bt, file))
@@ -19,15 +21,22 @@ def create_file(bt, file):
 
     lines.append("module load Anaconda3/2020.11\n")
     lines.append("source activate lisa\n")
-    if file == "optimistic":
-        lines.append('python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/fiducial/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --opt-flag -t ' + '{}\n'.format(bt))
-    else :
-        lines.append('python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(bt))
 
-    with open("scripts/{}_{}.sh".format(bt, file), "w") as f:
+    main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(bt)
+    if file == "optimistic":
+        main_line = main_line.replace("\n", " --opt-flag\n")
+    if simple_mw:
+        main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/simple_mw_{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --simple-mw -t ' + '{}\n'.format(bt)
+        script_file = script_file.replace(".sh", "_simplemw.sh")
+        print(script_file)
+    lines.append(main_line)
+
+    
+    with open(script_file, "w") as f:
         f.writelines(lines)
 
 
 for bt in ["BHBH", "BHNS", "NSNS"]:
+    create_file(bt, variations[0]["file"], simple_mw=True)
     for v in variations:
         create_file(bt, v["file"])
