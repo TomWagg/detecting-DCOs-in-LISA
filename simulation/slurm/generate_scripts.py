16dd3ce0bcce0d6fee3b4198579657d106644855
Tom Wagg <twagg@college.harvard.edu>
2021-06-19 17:26:12 -0400

    update to new files

--- simulation/slurm/generate_scripts.py
+++ simulation/slurm/generate_scripts.py
@@ -22,16 +22,16 @@ def create_file(bt, file, simple_mw=False):
     lines.append("module load Anaconda3/2020.11\n")
     lines.append("source activate lisa\n")
 
-    main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(bt)
+    main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(bt)
     if file == "optimistic":
         main_line = main_line.replace("\n", " --opt-flag\n")
     if simple_mw:
-        main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputReduced.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/simple_mw_{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --simple-mw -t ' + '{}\n'.format(bt)
+        main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/simple_mw_{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --simple-mw -t ' + '{}\n'.format(bt)
         script_file = script_file.replace(".sh", "_simplemw.sh")
         print(script_file)
     lines.append(main_line)
 
-    
+
     with open(script_file, "w") as f:
         f.writelines(lines)
 
