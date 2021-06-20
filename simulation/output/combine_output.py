9e845f3c531e30c815c2df6480d7856049d8e34f
Tom Wagg <twagg@college.harvard.edu>
2021-06-19 22:32:47 -0400

    update scripts for simple mw

--- simulation/output/combine_output.py
+++ simulation/output/combine_output.py
@@ -18,90 +18,97 @@ dtype = [("m_1", dt), ("m_2", dt), ("a_DCO", dt), ("e_DCO", dt),
          ("MT2_case", np.dtype(int)), ("a_ZAMS", dt), ("a_pre_SN2", dt),
          ("kick_1", dt), ("kick_2", dt), ("e_pre_SN2", dt)]
 
-runs = 50
-
 floor_path = "/n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/"
 
-# loop over all binary types and physics variations
-for bt in ["BHBH", "BHNS", "NSNS"]:
-    for v in range(len(variations)):
-        full_data = None
-        n_ten_year = np.array([], dtype=np.int)
-        total_weight = np.array([], dtype=np.float)
-        n_runs = 0
-        missing = []
+def combine_data(dco_type, variation, simple_mw=False, runs=50):
+    full_data = None
+    n_ten_year = np.array([], dtype=np.int)
+    total_weight = np.array([], dtype=np.float)
+    n_runs = 0
+    missing = []
 
-        # loop over each run
-        for i in range(1, runs + 1):
-            # check individual file exists
-            fname = "{}_{}_{}.h5".format(bt, variations[v]["file"], i)
-            if isfile(fname):
-                n_runs += 1
-
-                # open file and copy contents
-                with h5.File(fname, "r") as f:
-                    n_curr = f["simulation"].attrs["n_ten_year"].astype(np.int)
-                    n_ten_year = np.concatenate((n_ten_year, n_curr))
-
-                    total_temp = f["simulation"].attrs["total_MW_weight"]
-                    total_weight = np.concatenate((total_weight, total_temp))
-                    if full_data is None:
-                        full_data = f["simulation"][...].squeeze()
-                    else:
-                        add_data = f["simulation"][...].squeeze()
-                        prev_len = len(full_data)
-                        full_data.resize(prev_len + len(add_data))
-                        full_data[prev_len:] = add_data
-            else:
-                missing.append(i)
-
-        # as long as there is at least one file
-        if len(missing) != runs:
-            # let the user know which ones are currently missing
-            print(v, len(missing), missing)
-
-            # work out the formation channels
-            model = variations[v]["file"]
-            if model == "optimistic":
-                model = "fiducial"
-            floor_file = floor_path \
-                + "{}/COMPASOutputReduced.h5".format(model)
-            with h5.File(floor_file, "r") as floor:
-                channels = identify_formation_channels(full_data["seed"],
-                                                       floor)
-
-                dco_seeds = floor["doubleCompactObjects"]["seed"][...].squeeze()
-
-                index = np.argsort(dco_seeds)
-                sorted_dco_seeds = dco_seeds[index]
-                sorted_index = np.searchsorted(sorted_dco_seeds, full_data["seed"])
-
-                seeds_index = np.take(index, sorted_index, mode="clip")
-                supp_data = {
-                    "m_1_ZAMS": floor["doubleCompactObjects"]["M1ZAMS"][...].squeeze()[seeds_index],
-                    "m_2_ZAMS": floor["doubleCompactObjects"]["M2ZAMS"][...].squeeze()[seeds_index],
-                    "MT1_case": floor["doubleCompactObjects"]["PrimaryMTCase"][...].squeeze()[seeds_index],
-                    "MT2_case": floor["doubleCompactObjects"]["SecondaryMTCase"][...].squeeze()[seeds_index],
-                    "a_ZAMS": floor["doubleCompactObjects"]["separationInitial"][...].squeeze()[seeds_index],
-                    "a_pre_SN2": floor["doubleCompactObjects"]["separationPrior2ndSN"][...].squeeze()[seeds_index],
-                    "kick_1": floor["doubleCompactObjects"]["drawnKick1"][...].squeeze()[seeds_index],
-                    "kick_2": floor["doubleCompactObjects"]["drawnKick2"][...].squeeze()[seeds_index],
-                    "e_pre_SN2": floor["doubleCompactObjects"]["eccentricityPrior2ndSN"][...].squeeze()[seeds_index],
-                    "channel": channels
-                }
-
-            # write the rest of the files to a single file
-            fname = "../data/{}_{}_all.h5".format(bt, variations[v]["file"])
-            with h5.File(fname, "w") as file:
-                file.create_dataset("simulation", (np.sum(n_ten_year),),
-                                    dtype=dtype)
-                for col, types in dtype:
-                    if col in supp_data.keys():
-                        file["simulation"][col] = supp_data[col]
-                    else:
-                        file["simulation"][col] = full_data[col]
-                file["simulation"].attrs["n_ten_year"] = n_ten_year
-                file["simulation"].attrs["total_MW_weight"] = total_weight
-        # otherwise make sure that the user knows no data is present
+    # loop over each run
+    for i in range(1, runs + 1):
+        # check individual file exists
+        if simple_mw:
+            fname = "simple_mw_{}_{}_{}.h5".format(dco_type, variations[variation]["file"], i)
         else:
-            print("No data found for {}".format(variations[v]["long"]))
+            fname = "{}_{}_{}.h5".format(dco_type, variations[variation]["file"], i)
+        if isfile(fname):
+            n_runs += 1
+
+            # open file and copy contents
+            with h5.File(fname, "r") as f:
+                n_curr = f["simulation"].attrs["n_ten_year"].astype(np.int)
+                n_ten_year = np.concatenate((n_ten_year, n_curr))
+
+                total_temp = f["simulation"].attrs["total_MW_weight"]
+                total_weight = np.concatenate((total_weight, total_temp))
+                if full_data is None:
+                    full_data = f["simulation"][...].squeeze()
+                else:
+                    add_data = f["simulation"][...].squeeze()
+                    prev_len = len(full_data)
+                    full_data.resize(prev_len + len(add_data))
+                    full_data[prev_len:] = add_data
+        else:
+            missing.append(i)
+
+    # as long as there is at least one file
+    if len(missing) != runs:
+        # let the user know which ones are currently missing
+        print(variation, len(missing), missing)
+
+        # work out the formation channels
+        model = variations[variation]["file"]
+        if model == "optimistic":
+            model = "fiducial"
+        floor_file = floor_path + "{}/COMPASOutputCombined.h5".format(model)
+        with h5.File(floor_file, "r") as floor:
+            channels = identify_formation_channels(full_data["seed"],
+                                                    floor)
+
+            dco_seeds = floor["doubleCompactObjects"]["seed"][...].squeeze()
+
+            index = np.argsort(dco_seeds)
+            sorted_dco_seeds = dco_seeds[index]
+            sorted_index = np.searchsorted(sorted_dco_seeds, full_data["seed"])
+
+            seeds_index = np.take(index, sorted_index, mode="clip")
+            supp_data = {
+                "m_1_ZAMS": floor["doubleCompactObjects"]["M1ZAMS"][...].squeeze()[seeds_index],
+                "m_2_ZAMS": floor["doubleCompactObjects"]["M2ZAMS"][...].squeeze()[seeds_index],
+                "MT1_case": floor["doubleCompactObjects"]["PrimaryMTCase"][...].squeeze()[seeds_index],
+                "MT2_case": floor["doubleCompactObjects"]["SecondaryMTCase"][...].squeeze()[seeds_index],
+                "a_ZAMS": floor["doubleCompactObjects"]["separationInitial"][...].squeeze()[seeds_index],
+                "a_pre_SN2": floor["doubleCompactObjects"]["separationPrior2ndSN"][...].squeeze()[seeds_index],
+                "kick_1": floor["doubleCompactObjects"]["drawnKick1"][...].squeeze()[seeds_index],
+                "kick_2": floor["doubleCompactObjects"]["drawnKick2"][...].squeeze()[seeds_index],
+                "e_pre_SN2": floor["doubleCompactObjects"]["eccentricityPrior2ndSN"][...].squeeze()[seeds_index],
+                "channel": channels
+            }
+
+        # write the rest of the files to a single file
+        if simple_mw:
+            fname = "../data/simple_mw_{}_{}_all.h5".format(dco_type, variations[variation]["file"])
+        else:
+            fname = "../data/{}_{}_all.h5".format(dco_type, variations[variation]["file"])
+        with h5.File(fname, "w") as file:
+            file.create_dataset("simulation", (np.sum(n_ten_year),), dtype=dtype)
+            for col, types in dtype:
+                if col in supp_data.keys():
+                    file["simulation"][col] = supp_data[col]
+                else:
+                    file["simulation"][col] = full_data[col]
+            file["simulation"].attrs["n_ten_year"] = n_ten_year
+            file["simulation"].attrs["total_MW_weight"] = total_weight
+    # otherwise make sure that the user knows no data is present
+    else:
+        print("No data found for {}".format(variations[variation]["long"]))
+
+# loop over all binary types and physics variations
+for dco_type in ["BHBH", "BHNS", "NSNS"]:
+    for variation in range(len(variations)):
+        combine_data(dco_type, variation)
+        if variation == 0:
+            combine_data(dco_type, variation, simple_mw=True)
--- simulation/slurm/generate_scripts.py
+++ simulation/slurm/generate_scripts.py
@@ -3,32 +3,31 @@ sys.path.append("../src/")
 from variations import variations
 
 
-def create_file(bt, file, simple_mw=False):
-    script_file = "scripts/{}_{}.sh".format(bt, file)
+def create_file(dco_type, file, simple_mw=False):
+    script_file = "scripts/{}_{}.sh".format(dco_type, file)
 
     lines = []
     lines.append("#!/bin/bash\n")
-    lines.append("#SBATCH -J {}_{}\n".format(bt, file))
+    lines.append("#SBATCH -J {}_{}\n".format(dco_type, file))
     lines.append("#SBATCH -n 6\n")
     lines.append("#SBATCH -N 1\n")
     lines.append("#SBATCH -p serial_requeue\n")
     lines.append("#SBATCH --mem 6000\n")
     lines.append("#SBATCH -t 00-10:00\n")
-    lines.append("#SBATCH -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/slurm/logs/{}_{}_%A_%a.out\n".format(bt, file))
-    lines.append("#SBATCH -e /n/home09/twagg/detecting-DCOs-in-LISA/simulation/slurm/logs/{}_{}_%A_%a.err\n".format(bt, file))
+    lines.append("#SBATCH -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/slurm/logs/{}_{}_%A_%a.out\n".format(dco_type, file))
+    lines.append("#SBATCH -e /n/home09/twagg/detecting-DCOs-in-LISA/simulation/slurm/logs/{}_{}_%A_%a.err\n".format(dco_type, file))
     lines.append("#SBATCH --mail-user thomas.wagg@cfa.harvard.edu\n")
     lines.append("#SBATCH --mail-type ALL\n")
 
     lines.append("module load Anaconda3/2020.11\n")
     lines.append("source activate lisa\n")
 
-    main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(bt)
+    main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/{0}_{1}_'.format(dco_type, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 -t ' + '{}\n'.format(dco_type)
     if file == "optimistic":
         main_line = main_line.replace("\n", " --opt-flag\n")
     if simple_mw:
-        main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/simple_mw_{0}_{1}_'.format(bt, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --simple-mw -t ' + '{}\n'.format(bt)
+        main_line = 'python /n/home09/twagg/detecting-DCOs-in-LISA/simulation/src/simulate_DCO_detections.py -i /n/holystore01/LABS/berger_lab/Lab/fbroekgaarden/DATA/all_dco_legacy_CEbug_fix/{1}/COMPASOutputCombined.h5 -o /n/home09/twagg/detecting-DCOs-in-LISA/simulation/output/simple_mw_{0}_{1}_'.format(dco_type, file) + '"${SLURM_ARRAY_TASK_ID}".h5 -n 50 --simple-mw -t ' + '{}\n'.format(dco_type)
         script_file = script_file.replace(".sh", "_simplemw.sh")
-        print(script_file)
     lines.append(main_line)
 
 
@@ -36,7 +35,7 @@ def create_file(bt, file, simple_mw=False):
         f.writelines(lines)
 
 
-for bt in ["BHBH", "BHNS", "NSNS"]:
-    create_file(bt, variations[0]["file"], simple_mw=True)
+for dco_type in ["BHBH", "BHNS", "NSNS"]:
+    create_file(dco_type, variations[0]["file"], simple_mw=True)
     for v in variations:
-        create_file(bt, v["file"])
+        create_file(dco_type, v["file"])
