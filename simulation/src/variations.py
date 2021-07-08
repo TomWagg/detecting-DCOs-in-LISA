a7da352ebfaf4f53019d9313d7a3e6a9ce1ded47
Tom Wagg <twagg@college.harvard.edu>
2021-07-08 11:44:23 -0400

    update variations (not colours though)

--- simulation/src/variations.py
+++ simulation/src/variations.py
@@ -43,57 +43,78 @@ variations = np.array(
             "colour": plt.get_cmap("tab20")(2)
         },
         {
-            "file": "alpha0_5",
+            "file": "unstableCaseBB_opt",
             "short": "F",
+            "med": "unstable\ncase BB optimistic",
+            "long": "Unstable case BB mass transfer with optimistic CE scenario",
+            "colour": plt.get_cmap("tab20")(2)
+        },
+        {
+            "file": "alpha0_1",
+            "short": "G",
+            "med": r"$\alpha_{\rm CE}=0.1$",
+            "long": r"CE efficiency $\alpha = 0.1$",
+            "colour": plt.get_cmap("tab20b")(11)
+        },
+        {
+            "file": "alpha0_5",
+            "short": "H",
             "med": r"$\alpha_{\rm CE}=0.5$",
             "long": r"CE efficiency $\alpha = 0.5$",
             "colour": plt.get_cmap("tab20b")(11)
         },
         {
             "file": "alpha2_0",
-            "short": "G",
+            "short": "I",
             "med": r"$\alpha_{\rm CE}=2.0$",
             "long": r"CE efficiency $\alpha = 2.0$",
             "colour": plt.get_cmap("tab20b")(10)
         },
         {
+            "file": "alpha10",
+            "short": "J",
+            "med": r"$\alpha_{\rm CE}=10.0$",
+            "long": r"CE efficiency $\alpha = 10.0$",
+            "colour": plt.get_cmap("tab20b")(10)
+        },
+        {
             "file": "optimistic",
-            "short": "H",
+            "short": "K",
             "med": "optimistic\nCE",
             "long": "Optimistic CE",
             "colour": "darkgoldenrod"
         },
         {
             "file": "rapid",
-            "short": "I",
+            "short": "L",
             "med": "rapid SN",
             "long": "Fryer rapid prescription",
             "colour": "lightseagreen"
         },
         {
             "file": "maxNSmass2_0",
-            "short": "J",
+            "short": "M",
             "med": r"max $m_{\rm NS}$" + "\n" + r"$2.0 \, {\rm M_{\odot}}$",
             "long": r"Maximum neutron star mass = 2.0 ${\rm M_{\odot}}$",
             "colour": plt.get_cmap("tab20")(1/20)
         },
         {
             "file": "maxNSmass3_0",
-            "short": "K",
+            "short": "N",
             "med": r"max $m_{\rm NS}$" + "\n" + r"$3.0 \, {\rm M_{\odot}}$",
             "long": r"Maximum neutron star mass = 3.0 ${\rm M_{\odot}}$",
             "colour": plt.get_cmap("tab20")(0/20)
         },
         {
             "file": "noPISN",
-            "short": "L",
+            "short": "O",
             "med": "no PISN",
             "long": "No pair instability supernova",
             "colour": "deepskyblue"
         },
         {
             "file": "ccSNkick_100km_s",
-            "short": "M",
+            "short": "P",
             "med": r"$\sigma_{\rm cc}$" + "\n" + r"$100 \, {\rm km s^{-1}}$",
             "long": "SN kick dispersion "
                      + r"$\sigma_{\rm RMS}^{\rm 1D} = 100 \ {\rm km\ s^{-1}}$",
@@ -101,7 +122,7 @@ variations = np.array(
         },
         {
             "file": "ccSNkick_30km_s",
-            "short": "N",
+            "short": "Q",
             "med": r"$\sigma_{\rm cc}$" + "\n" + r"$30 \, {\rm km s^{-1}}$",
             "long": "SN kick dispersion "
                      + r"$\sigma_{\rm RMS}^{\rm 1D} = 30 \ {\rm km\ s^{-1}}$",
@@ -109,21 +130,21 @@ variations = np.array(
         },
         {
             "file": "noBHkick",
-            "short": "O",
+            "short": "R",
             "med": "no BH\nkicks",
             "long": "No BH kicks",
             "colour": "darkgreen"
         },
         {
             "file": "wolf_rayet_multiplier_0_1",
-            "short": "P",
+            "short": "S",
             "med": r"$f_{\rm WR} = 0.1$",
             "long": r"Wolf Rayet wind multiplier $= 0.1$",
             "colour": "thistle"
         },
         {
             "file": "wolf_rayet_multiplier_5",
-            "short": "Q",
+            "short": "T",
             "med": r"$f_{\rm WR} = 5$",
             "long": r"Wolf Rayet wind multiplier $= 5.0$",
             "colour": "purple"
