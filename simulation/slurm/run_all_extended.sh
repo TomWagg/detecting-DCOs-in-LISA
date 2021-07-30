cfc2770da386c4cbfc9ede63d50c270839d9e56f
Tom Wagg <twagg@college.harvard.edu>
2021-07-30 20:58:33 +0100

    remove missing models

--- simulation/slurm/run_all_extended.sh
+++ simulation/slurm/run_all_extended.sh
@@ -3,7 +3,7 @@ python generate_scripts.py
 
 for type in BHBH BHNS NSNS
 do
-    for variation in fiducial massTransferEfficiencyFixed_0_25 massTransferEfficiencyFixed_0_5 massTransferEfficiencyFixed_0_75 unstableCaseBB unstableCaseBB_opt alpha0_1 alpha0_5 alpha2_0 alpha10 optimistic rapid maxNSmass2_0 maxNSmass3_0 noPISN ccSNkick_100km_s ccSNkick_30km_s noBHkick wolf_rayet_multiplier_0_1 wolf_rayet_multiplier_5
+    for variation in fiducial massTransferEfficiencyFixed_0_25 massTransferEfficiencyFixed_0_5 massTransferEfficiencyFixed_0_75 unstableCaseBB unstableCaseBB_opt alpha0_1 alpha0_5 alpha2_0 alpha10 optimistic rapid maxNSmass2_0 maxNSmass3_0 noPISN ccSNkick_100km_s ccSNkick_30km_s wolf_rayet_multiplier_0_1
     do
         sbatch --array=1-50 scripts/${type}_${variation}_10yr.sh
         sleep 1
